bc08e15ab0fa74e382da2faa12097bd5454091f2
NailZI <duble76@gmail.com>
2022-03-07 17:49:17 +0300

    pulse les_2

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
-let answer = prompt("Y/N?", "")
-console.log(answer)
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+      });
+  });
